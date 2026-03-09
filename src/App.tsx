1bc3f2c5bd7dfa1a123364b54b961022be444811
spagnoll1andrea <andreaspagnolli40@gmail.com>
2026-03-10 00:22:44 +0100

    step 1

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/music-riva-revival/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/chi-siamo" element={<ChiSiamoPage />} />
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/music-riva-revival/",
   server: {
     host: "::",
     port: 8080,
